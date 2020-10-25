ce6b9646361212ddeb7002b2d2af4a98eee53fbf
Ivan Smetanin <smetanin23@yandex.ru>
2020-10-25 14:58:18 +0300

    Initial Commit

---
+++ Jobs/JobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsApp.swift
+//  Jobs
+//
+//  Created by i.smetanin on 25.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct JobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
